468fbc3f7d2a3ad18e46e2d07bc3a38a69759d7c
Edoardo Tagliani <edoardo.tagliani@itsrizzoli.it>
2023-12-19 11:21:38 +0100

    funzione somma

--- src/App.java
+++ src/App.java
@@ -11,6 +11,14 @@ public class App {
             arr[i]=scanner.nextInt();
         }
 
-        
+    public static int Somma(int[] arr){
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            sum = sum + arr[i];
+        }
+
+        return sum;
+
+    }
 
 }
