06eb1b7d2de8e1315515983c0ee6273a41314998
Edoardo Tagliani <edoardo.tagliani@itsrizzoli.it>
2023-12-19 11:27:05 +0100

    fine

--- src/App.java
+++ src/App.java
@@ -7,7 +7,7 @@ public class App {
         int n = scanner.nextInt();
         int[] arr = new int[n];
         
-        for (int i=0; i<=n; i++){
+        for (int i=0; i<n; i++){
             arr[i]=scanner.nextInt();
         }
         
